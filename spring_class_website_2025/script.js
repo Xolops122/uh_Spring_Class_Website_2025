565b0a97d4908fe19e06335e8973489f0ca1ac4a
Xolops122 <alanrc199@gmail.com>
2025-03-18 21:23:37 -0700

    added all the stuff

---
+++ spring_class_website_2025/script.js
@@ -0,0 +1,14 @@
+// Function to get the browser window width
+function getWindowWidth() {
+    var width = window.innerWidth;
+    return width;
+}
+
+function getWindowHeight() {
+    var Height = window.innerHeight;
+    return Height;
+}
+
+// Store the width in a variable
+var browserWidth = getWindowWidth();
+var browserHeight = getWindowHeight();
